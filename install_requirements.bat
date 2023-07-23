ab8bc3d8f665d24b396dde55b4976ab971a47f5e
Giacomo Vallasciani <127063688+Sc1anso@users.noreply.github.com>
2023-07-23 15:41:34 +0200

    windows and linux requirements installers

---
+++ install_requirements.bat
@@ -0,0 +1,6 @@
+@echo off
+
+:: Add any other options you may need, e.g., --user, --upgrade, etc.
+pip install -r ./main_req/requirements.txt
+pip install -r ./server_req/requirements.txt
+:: Add more requirements files if needed
